808a6355dcbf19e4bfbd7fadf59e14a1792798ed
Irving Wang <irvingywang@gmail.com>
2024-03-31 13:47:08 -0400

    document classes

--- Chat.java
+++ Chat.java
@@ -1,12 +1,20 @@
 import java.util.ArrayList;
 import java.util.HashMap;
-
+/**
+ * Represents a chat which has a list of members and messages.
+ */
 public class Chat implements ChatInterface {
     private String id;
     private String name;
     private HashMap<String, User> members; //username, user
     private ArrayList<Message> messages;
 
+    /**
+     * Constructs a new Chat object.
+     *
+     * @param name  - the name of the chat
+     * @param users - the list of users to be added to the chat
+     */
     public Chat(String name, ArrayList<User> users) {
         this.name = name;
         members = new HashMap<>();
@@ -16,6 +24,13 @@ public class Chat implements ChatInterface {
         }
     }
 
+    /**
+     * Adds a message to the chat.
+     *
+     * @param sender  - the sender of the message
+     * @param message - the message to be added
+     * @return true if the message was added, false otherwise
+     */
     @Override
     public boolean addMessage(User sender, Message message) {
         if (!members.containsKey(sender.getUsername())) {
@@ -30,16 +45,32 @@ public class Chat implements ChatInterface {
         return true;
     }
 
+    /**
+     * Returns the list of messages in the chat.
+     *
+     * @return the list of messages
+     */
     @Override
     public ArrayList<Message> getMessages() {
         return messages;
     }
 
+    /**
+     * Returns the map of members in the chat.
+     *
+     * @return the map of members
+     */
     @Override
     public HashMap<String, User> getMembers() {
         return members;
     }
 
+    /**
+     * Checks if a user is a member of the chat.
+     *
+     * @param user - the user to check
+     * @return true if the user is a member, false otherwise
+     */
     @Override
     public boolean isMember(User user) {
         return members.containsKey(user.getUsername());
--- GroupChat.java
+++ GroupChat.java
@@ -1,11 +1,25 @@
 import java.util.ArrayList;
-
+/**
+ * Represents a group chat with a variable amount of members.
+ */
 public class GroupChat extends Chat {
 
+    /**
+     * Constructs a new GroupChat object.
+     *
+     * @param name  - the name of the chat
+     * @param users - the list of users to be added to the chat
+     */
     public GroupChat(String name, ArrayList<User> users) {
         super(name, users);
     }
 
+    /**
+     * Adds a member to the chat.
+     *
+     * @param user - the user to be added
+     * @return true if the user was added, false otherwise
+     */
     public boolean addMember(User user) {
         if (super.getMembers().containsKey(user.getUsername())) {
             return false;
@@ -14,6 +28,12 @@ public class GroupChat extends Chat {
         return true;
     }
 
+    /**
+     * Removes a member from the chat.
+     *
+     * @param user - the user to be removed
+     * @return true if the user was removed, false otherwise
+     */
     public boolean removeMember(User user) {
         if (!super.getMembers().containsKey(user.getUsername())) {
             return false;
