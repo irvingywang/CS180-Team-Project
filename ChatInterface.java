4ac5b3501b9951ab4c6eb622913f39545bae7fd3
kvankwan <156548681+kvankwan@users.noreply.github.com>
2024-04-13 12:49:45 -0400

    made changes in this

--- ChatInterface.java
+++ ChatInterface.java
@@ -2,21 +2,21 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 /**
- * Project05 -- ChatInterface
- *
+ * Project05 -- ChatInterface*
  * Creates an interface for the chat class.
  *
  * @author Amir Elnashar, L08
  * @author Irving Wang, L08
  * @author Jack Kim, L08
  * @author John Guan, L08
- *
  * @version April 1, 2024
- *
- */
+ **/
 public interface ChatInterface {
     boolean addMessage(User sender, Message message);
+
     ArrayList<Message> getMessages();
-    HashMap<String, User> getMembers();
+
+    ArrayList<User> getMembers();
+
     boolean isMember(User user);
 }
