a1c23aa050598b43ebb899314de903e591f3c481
Irving Wang <irvingywang@gmail.com>
2024-04-16 15:38:18 -0400

    refactor source

--- src/Server.java
+++ src/Server.java
@@ -2,7 +2,6 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.List;
-import java.util.Map;
 
 /**
  * Project05 -- Server
--- src/UserInterface.java
+++ src/UserInterface.java
@@ -1,5 +1,3 @@
-import java.util.ArrayList;
-
 /**
  * Project05 -- UserInterface
  *
